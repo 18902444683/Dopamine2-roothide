cd487943b06e00c373c4d06d3a5b083cc64c94de
@skye // pa1n <sonicgames772@gmail.com>
2025-03-23 02:07:11 -0300

    DOEnvironmentManager.m: 18.5
    dummy

--- Application/Dopamine/Jailbreak/DOEnvironmentManager.m
+++ Application/Dopamine/Jailbreak/DOEnvironmentManager.m
@@ -218,10 +218,10 @@ int reboot3(uint64_t flags, ...);
 - (NSString *)versionSupportString
 {
     if ([self isArm64e]) {
-        return @"iOS 15.0 - 16.5.1 (arm64e)";
+        return @"iOS 15.0 - 18.5 (arm64e-roothide)";
     }
     else {
-        return @"iOS 15.0 - 16.6.1 (arm64)";
+        return @"iOS 15.0 - 16.6.1 (arm64-roothide)";
     }
 }
 
